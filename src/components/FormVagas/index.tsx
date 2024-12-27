c632da3eee770a8ff7258c6e67f61437ed7f4149
igor_programmer <99368475+igorprogrammer93@users.noreply.github.com>
2024-12-27 15:16:08 +0000

    commit stylecomponentes

--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,31 +1,65 @@
-import { FormEvent, useState } from 'react'
-
-import styles from './FormVagas.module.css'
+import { FormEvent, useState } from "react";
+import styled from "styled-components";
 
 type Props = {
-  aoPesquisar: (termo: string) => void
-}
+  aoPesquisar: (termo: string) => void;
+};
+
+// Styled Components com tipos explícitos
+const Form = styled.form.attrs(() => ({
+  role: "form", // Apenas para boas práticas de acessibilidade
+}))<React.ComponentProps<"form">>`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  background-color: var(--cor-secundaria);
+  padding: 32px;
+  border-radius: 12px;
+  margin-top: 40px;
+`;
+
+const Input = styled.input.attrs(() => ({
+  type: "search",
+}))<React.ComponentProps<"input">>`
+  padding: 0 16px;
+  outline-color: var(--cor-principal);
+  font-size: 16px;
+`;
+
+const Button = styled.button.attrs(() => ({
+  type: "submit",
+}))<React.ComponentProps<"button">>`
+  background-color: var(--cor-principal);
+  border: 1px solid var(--cor-principal);
+  height: 40px;
+  padding: 0 16px;
+  font-size: 18px;
+  color: var(--cor-secundaria);
+  margin-left: 8px;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
 
 const FormVagas = ({ aoPesquisar }: Props) => {
-  const [termo, setTermo] = useState<string>('')
+  const [termo, setTermo] = useState<string>("");
 
   const aoEnviarForm = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    aoPesquisar(termo.toLocaleLowerCase())
-  }
+    e.preventDefault();
+    aoPesquisar(termo.toLocaleLowerCase());
+  };
 
   return (
-    <form className={styles.form} onSubmit={aoEnviarForm}>
-      <input
-        className={styles.campo}
+    <Form onSubmit={aoEnviarForm}>
+      <Input
         placeholder="Front-end, fullstack, node, design"
         onChange={(e) => setTermo(e.target.value)}
-        type="search"
       />
-      <button className={styles.btnPesquisar} type="submit">
-        Pesquisar
-      </button>
-    </form>
-  )
-}
-export default FormVagas
+      <Button>Pesquisar</Button>
+    </Form>
+  );
+};
+
+export default FormVagas;
+
